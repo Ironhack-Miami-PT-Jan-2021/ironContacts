b1fc6d103350231a70e9e42c61688e884233ea67
Nicholas Borbe <nickborbe@Nicholass-Mac-mini.local>
2021-05-06 21:29:44 -0400

    review

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Contacts from './components/contacts/Contacts'
+
+function App() {
+  return (
+    <div>
+      <Contacts/>
+    </div>
+  );
+}
+
+export default App;
