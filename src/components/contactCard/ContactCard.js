12b39efa9cd257fe94d264162af11537fa57ec9f
Nicholas Borbe <nickborbe@Nicholass-Mac-mini.local>
2021-05-13 21:14:29 -0400

    commented out error

--- src/components/contactCard/ContactCard.js
+++ src/components/contactCard/ContactCard.js
@@ -8,14 +8,14 @@ class ContactCard extends React.Component {
     render(){
         const {name, pictureUrl: pic, popularity, id} = this.props;
         return(
+            <div className="card"> 
             <Link to={`/contacts/${id}`}>
-                <div className="card"> 
                     <h3>{name}</h3>
+            </Link>
                     <img src={pic} />
                     <p>Popularity: {popularity}</p>
                     <button onClick = {()=>{this.props.delete(id)}}>Delete</button>
                 </div>
-            </Link>
         )
     }
 
